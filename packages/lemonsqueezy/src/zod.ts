fd7effacaf0c0dc80ddfebd876af60d26435886f
nurodev <me@nuro.dev>
2023-03-19 00:48:19 +0000

    Updated Zod exports

--- packages/lemonsqueezy/src/zod.ts
+++ packages/lemonsqueezy/src/zod.ts
@@ -2,3 +2,8 @@ export { DataType } from "./_shared/_shared.validators";
 export * from "./checkout/checkout.validators";
 export * from "./discount/discount.validators";
 export * from "./file/file.validators";
+export * from "./licenseKey/licenseKey.validators";
+export * from "./licenseKeyInstance/licenseKeyInstance.validators";
+export * from "./order/order.validators";
+export * from "./orderItem/orderItem.validators";
+export * from "./product/product.validators";
