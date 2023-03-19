603963c6f027cc910fe2e2d24ee2b6c9cd1b207d
nurodev <me@nuro.dev>
2023-03-19 23:18:30 +0000

    Added basic LogSnag example script

---
+++ examples/logsnag.ts
@@ -0,0 +1,36 @@
+import { createTypeLevelClient } from "untypeable";
+import { join } from "node:path";
+
+import "dotenv/config";
+
+import type { LogSnagRouter } from "../packages/logsnag";
+
+export default async function main() {
+  const client = createTypeLevelClient<LogSnagRouter>(
+    async (path, input = {}) => {
+      const { apiVersion = "v1", ...options } = input;
+
+      const url = new URL(join(apiVersion, path), "https://api.logsnag.com");
+
+      const response = await fetch(url.href, {
+        body: JSON.stringify(options),
+        headers: {
+          Authorization: `Bearer ${process.env.LOGSNAG_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+
+      return await response.json();
+    }
+  );
+
+  await client("/log", {
+    project: "nuro",
+    channel: "untyped",
+    event: "Test",
+    description: "A basic test event from the `@untyped/logsnag` package",
+    icon: "👋",
+    notify: false,
+  });
+}
