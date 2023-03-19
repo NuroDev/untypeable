878b52b1dcd075d052ee283bf914c5be7c0f6dcc
nurodev <me@nuro.dev>
2023-03-19 00:34:47 +0000

    Added initial Lemon Squeezy order validators

--- packages/lemonsqueezy/src/order/order.test.ts
+++ packages/lemonsqueezy/src/order/order.test.ts
@@ -1,34 +1,40 @@
 import { describe, it, expect } from "vitest";
 
 import { DataType } from "../_shared/_shared.types";
+import { OrderSchema, OrdersSchema } from "./order.validators";
 import { useTestClient } from "../_shared/_shared.util";
 
 describe.concurrent("Lemon Squeezy - Order", () => {
   const client = useTestClient();
 
   it("GET - /orders", async () => {
-    // const orderItems = await client("/orders", "GET");
+    // const orders = await client("/orders", "GET");
 
-    // expect(orderItems).toBeDefined();
-    // expect(orderItems.data).toBeDefined();
-    // expect(orderItems.data.length).toBeGreaterThanOrEqual(1);
-    // expect(orderItems.data.at(0)).toBeDefined();
-    // expect(orderItems.data.at(0)?.type).toBe(DataType.orders);
-    // expect(orderItems.errors).toBeUndefined();
+    // expect(orders).toBeDefined();
+    // expect(orders.data).toBeDefined();
+    // expect(orders.data.length).toBeGreaterThanOrEqual(1);
+    // expect(orders.data.at(0)).toBeDefined();
+    // expect(orders.data.at(0)?.type).toBe(DataType.orders);
+    // expect(orders.errors).toBeUndefined();
+
+    // expect(OrdersSchema.safeParse(orders).success).toBe(true);
 
     expect(true).toBe(true);
   });
 
   it("GET - /orders/:id", async () => {
-    // const orderItems = await client("/orders", "GET");
-    // const orderItem = await client(`/orders/:id`, "GET", {
-    //   id: orderItems.data.at(0)!.id,
+    // const orders = await client("/orders", "GET");
+    // const order = await client(`/orders/:id`, "GET", {
+    //   id: orders.data.at(0)!.id,
     // });
 
-    // expect(orderItem).toBeDefined();
-    // expect(orderItem.data).toBeDefined();
-    // expect(orderItem.data.type).toBe(DataType.orders);
-    // expect(orderItem.errors).toBeUndefined();
+    // expect(order).toBeDefined();
+    // expect(order.data).toBeDefined();
+    // expect(order.data.type).toBe(DataType.orders);
+    // expect(order.errors).toBeUndefined();
+
+    // expect(OrdersSchema.safeParse(orders).success).toBe(true);
+    // expect(OrderSchema.safeParse(order).success).toBe(true);
 
     expect(true).toBe(true);
   });
