1be9fe152c98707ac8a7983582196d3454240e59
nurodev <me@nuro.dev>
2023-03-20 22:47:14 +0000

    Split routers based on HTTP method args

--- packages/jsonplaceholder/src/index.ts
+++ packages/jsonplaceholder/src/index.ts
@@ -22,36 +22,41 @@ import type { Post, PostParams, Posts, PostsParams } from "./post/post.types";
 import type { Todo, TodoParams, Todos, TodosParams } from "./todo/todo.types";
 import type { User, UserParams, Users, UsersParams } from "./user/user.types";
 
-const u = initUntypeable().pushArg<
-  "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
->();
+const u = initUntypeable();
 
-const router = u.router({
+const allResourcesRouter = u.pushArg<"GET" | "POST">().router({
   "/albums": { GET: u.input<AlbumsParams>().output<Albums>() },
-  "/albums/:id": { GET: u.input<AlbumParams>().output<Album>() },
-  "/albums/:id/comments": { GET: u.input<AlbumParams>().output<Comments>() },
-
   "/comments": { GET: u.input<CommentsParams>().output<Comments>() },
-  "/comments/:id": { GET: u.input<CommentParams>().output<Comment>() },
-  "/comments/:id/comments": {
-    GET: u.input<CommentParams>().output<Comments>(),
-  },
-
   "/photos": { GET: u.input<PhotosParams>().output<Photos>() },
-  "/photos/:id": { GET: u.input<PhotoParams>().output<Photo>() },
-  "/photos/:id/comments": { GET: u.input<PhotoParams>().output<Comments>() },
-
   "/posts": { GET: u.input<PostsParams>().output<Posts>() },
-  "/posts/:id": { GET: u.input<PostParams>().output<Post>() },
-  "/posts/:id/comments": { GET: u.input<PostParams>().output<Comments>() },
-
   "/todos": { GET: u.input<TodosParams>().output<Todos>() },
-  "/todos/:id": { GET: u.input<TodoParams>().output<Todo>() },
-  "/todos/:id/comments": { GET: u.input<TodoParams>().output<Comments>() },
-
   "/users": { GET: u.input<UsersParams>().output<Users>() },
-  "/users/:id": { GET: u.input<UserParams>().output<User>() },
-  "/users/:id/comments": { GET: u.input<UserParams>().output<Comments>() },
 });
+
+const singleResourceRouter = u
+  .pushArg<"GET" | "PUT" | "PATCH" | "DELETE">()
+  .router({
+    "/albums/:id": { GET: u.input<AlbumParams>().output<Album>() },
+    "/albums/:id/comments": { GET: u.input<AlbumParams>().output<Comments>() },
+
+    "/comments/:id": { GET: u.input<CommentParams>().output<Comment>() },
+    "/comments/:id/comments": {
+      GET: u.input<CommentParams>().output<Comments>(),
+    },
+
+    "/photos/:id": { GET: u.input<PhotoParams>().output<Photo>() },
+    "/photos/:id/comments": { GET: u.input<PhotoParams>().output<Comments>() },
+
+    "/posts/:id": { GET: u.input<PostParams>().output<Post>() },
+    "/posts/:id/comments": { GET: u.input<PostParams>().output<Comments>() },
+
+    "/todos/:id": { GET: u.input<TodoParams>().output<Todo>() },
+    "/todos/:id/comments": { GET: u.input<TodoParams>().output<Comments>() },
+
+    "/users/:id": { GET: u.input<UserParams>().output<User>() },
+    "/users/:id/comments": { GET: u.input<UserParams>().output<Comments>() },
+  });
+
+const router = allResourcesRouter.merge(singleResourceRouter);
 
 export type JSONPlaceholderRouter = typeof router;
