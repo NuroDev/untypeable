7569c16e21f3104c213ffe45456bb47f9cf09817
nurodev <me@nuro.dev>
2023-03-21 00:31:51 +0000

    Updated JSONPlaceholder test client handler to add filter parameters

--- packages/jsonplaceholder/tests/_client.ts
+++ packages/jsonplaceholder/tests/_client.ts
@@ -17,6 +17,23 @@ export function useTestClient() {
         "https://jsonplaceholder.typicode.com"
       );
 
+      // Add filter parameters to the URL if it's a
+      // `GET` method and an input is provided
+      if (method === "GET" && input) {
+        // Get all path parameters from the path, EG: `/:id` => `id`
+        const pathParams = [...path.matchAll(/\/:(\w+)/g)].map(
+          ([, param]) => param
+        );
+        // Remove all path parameters from the input
+        const strippedInput = Object.fromEntries(
+          Object.entries(input).filter((e) => !pathParams.includes(e[0]))
+        );
+        // Append all remaining input to the URL
+        Object.entries(strippedInput).forEach(([key, value]) =>
+          url.searchParams.append(key, value as string)
+        );
+      }
+
       const response = await fetch(url.href, {
         body: method === "GET" ? undefined : JSON.stringify(input),
         method,
