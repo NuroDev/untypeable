7a4768775714ab6201b32753305892bce64bec9c
nurodev <me@nuro.dev>
2023-04-11 01:45:25 +0100

    Minor code cleanup

--- packages/hackernews/src/liveData/liveData.types.ts
+++ packages/hackernews/src/liveData/liveData.types.ts
@@ -15,15 +15,10 @@ import type {
 export type MaxItemId = z.infer<typeof MaxItemIdSchema>;
 
 export type AskStories = z.infer<typeof AskStoriesSchema>;
-
 export type BestStories = z.infer<typeof BestStoriesSchema>;
-
 export type JobStories = z.infer<typeof JobStoriesSchema>;
-
 export type NewStories = z.infer<typeof NewStoriesSchema>;
-
 export type ShowStories = z.infer<typeof ShowStoriesSchema>;
-
 export type TopStories = z.infer<typeof TopStoriesSchema>;
 
 export type Updates = z.infer<typeof UpdatesSchema>;
