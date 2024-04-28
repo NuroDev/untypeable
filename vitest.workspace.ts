435efb84fe26b04f78b364a5e83e763448d41b9b
nurodev <ben@nuro.dev>
2024-04-28 12:42:26 +0100

    Temp disabled swapi package workspace tests

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,1 +1,9 @@
-export default ["packages/*"];
+// TODO(@nurodev): Revert to `packages/*` once swapi.dev SSL cert is fixed.
+export default [
+  "packages/hackernews/",
+  "packages/jsonplaceholder/",
+  "packages/lil.apis/",
+  "packages/logsnag/",
+  "packages/spacex/",
+  // "packages/swapi/",
+];
